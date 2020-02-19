74ebe581f987b4ca7663ab7bc4f6f1ae96b9d625
Ewa Niezgoda <eniezgo@gmail.com>
2020-02-19 21:54:05 +0100

    database structure

---
+++ heaven_and_hell.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS courses (
+course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+title VARCHAR,
+ects INTEGER,
+price decimal,
+start_date DATE,
+end_date DATE,
+type VARCHAR);
+
+CREATE TABLE IF NOT EXISTS online_course (
+course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+url VARCHAR);
+
+CREATE TABLE IF NOT EXISTS onsite_course (
+course_id INTEGER primary key AUTOINCREMENT,
+address VARCHAR,
+duartion INTEGER);
+
+CREATE TABLE IF NOT EXISTS departement (
+departement_id INTEGER PRIMARY KEY AUTOINCREMENT,
+departement_name VARCHAR,
+address VARCHAR,
+manager VARCHAR,
+budget INTEGER);
+
+
+CREATE TABLE IF NOT EXISTS employees (
+employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name VARCHAR,
+last_name VARCHAR,
+start_date DATE,
+PESEL INTEGER,
+telephone INTEGER,
+address VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS stuent_grades (
+student_id INTEGER,
+course_id INTEGER,
+ects INTEGER,
+grade INTEGER);
+
+
+CREATE TABLE IF NOT EXISTS students (
+student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name VARCHAR,
+last_name VARCHAR,
+address VARCHAR,
+pesel INTEGER,
+telephone INTEGER);
+
+
+
+
