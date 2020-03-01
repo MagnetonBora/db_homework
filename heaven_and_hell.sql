5980b82c8f2f42b238517fb2cd22d153f1524efa
Roman Dryndik <dryndikroman@gmail.com>
2020-03-01 15:53:45 +0100

    Refactor
    Format SQL db creation script.
    
    NOTE: There are no relations between tables, e.g the constraints haven't been implemented.

--- heaven_and_hell.sql
+++ heaven_and_hell.sql
@@ -1,54 +1,54 @@
 CREATE TABLE IF NOT EXISTS courses (
-course_id INTEGER PRIMARY KEY AUTOINCREMENT,
-title VARCHAR,
-ects INTEGER,
-price decimal,
-start_date DATE,
-end_date DATE,
-type VARCHAR);
+  course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR,
+  ects INTEGER,
+  price decimal,
+  start_date DATE,
+  end_date DATE,
+  type VARCHAR
+);
 
 CREATE TABLE IF NOT EXISTS online_course (
-course_id INTEGER PRIMARY KEY AUTOINCREMENT,
-url VARCHAR);
+  course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  url VARCHAR
+);
 
 CREATE TABLE IF NOT EXISTS onsite_course (
-course_id INTEGER primary key AUTOINCREMENT,
-address VARCHAR,
-duartion INTEGER);
+  course_id INTEGER primary key AUTOINCREMENT,
+  address VARCHAR,
+  duartion INTEGER
+);
 
 CREATE TABLE IF NOT EXISTS departement (
-departement_id INTEGER PRIMARY KEY AUTOINCREMENT,
-departement_name VARCHAR,
-address VARCHAR,
-manager VARCHAR,
-budget INTEGER);
-
+  departement_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  departement_name VARCHAR,
+  address VARCHAR,
+  manager VARCHAR,
+  budget INTEGER
+);
 
 CREATE TABLE IF NOT EXISTS employees (
-employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
-first_name VARCHAR,
-last_name VARCHAR,
-start_date DATE,
-PESEL INTEGER,
-telephone INTEGER,
-address VARCHAR
+  employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  start_date DATE,
+  PESEL INTEGER,
+  telephone INTEGER,
+  address VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS student_grades (
-student_id INTEGER,
-course_id INTEGER,
-ects INTEGER,
-grade INTEGER);
-
+  student_id INTEGER,
+  course_id INTEGER,
+  ects INTEGER,
+  grade INTEGER
+);
 
 CREATE TABLE IF NOT EXISTS students (
-student_id INTEGER PRIMARY KEY AUTOINCREMENT,
-first_name VARCHAR,
-last_name VARCHAR,
-address VARCHAR,
-pesel INTEGER,
-telephone INTEGER);
-
-
-
-
+  student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  address VARCHAR,
+  PESEL INTEGER,
+  telephone INTEGER
+);
