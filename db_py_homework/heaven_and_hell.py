0cb11272980d5e60bd124313dfc8230ab09a984c
Roman Dryndik <dryndikroman@gmail.com>
2020-03-01 16:07:26 +0100

    Fix not working code
    This code didn't work. Queries in the "queries" list should be separated with comma. Otherwise Python will concatenate those strings into ONE SQL statement. SqlAlchemy, on the other hand, won't be able to process such statement since it contains MULTIPLE CREATE operations.

--- db_py_homework/heaven_and_hell.py
+++ db_py_homework/heaven_and_hell.py
@@ -18,39 +18,46 @@ if __name__ == '__main__':
     url = 'sqlite:///:memory:'
     engine = create_engine(url)
 
-queries = ['CREATE TABLE IF NOT EXISTS courses (course_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
-           'title VARCHAR, ects INTEGER, \n'
-           'price decimal, \n'
-           'start_date DATE,\n '
-           'end_date DATE, \n'
-           'type VARCHAR);,'
-           'CREATE TABLE IF NOT EXISTS online_course (course_id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
-           'url VARCHAR);,'
-           'CREATE TABLE IF NOT EXISTS onsite_course (course_id INTEGER primary key AUTOINCREMENT,\n'
-           'address VARCHAR, \n'
-           'duartion INTEGER);,'
-           'CREATE TABLE IF NOT EXISTS departement (departement_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
-           'departement_name VARCHAR, \n'
-           'address VARCHAR, \n'
-           'manager VARCHAR, \n'
-           'budget INTEGER);,'
-           'CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
-           'first_name VARCHAR, \n'
-           'last_name VARCHAR, \n'
-           'start_date DATE, \n'
-           'PESEL INTEGER, \n'
-           'telephone INTEGER, \n'
-           'address VARCHAR);,'
-           'CREATE TABLE IF NOT EXISTS stuent_grades (student_id INTEGER, \n'
-           'course_id INTEGER,\n'
-           'ects INTEGER,\n'
-           'grade INTEGER);,'
-           'CREATE TABLE IF NOT EXISTS students (student_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
-           'first_name VARCHAR,\n'
-           'last_name VARCHAR, \n'
-           'address VARCHAR, \n'
-           'pesel INTEGER, \n'
-           'telephone INTEGER);']
+queries = [
+    'CREATE TABLE IF NOT EXISTS courses (course_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
+    'title VARCHAR, ects INTEGER, \n'
+    'price decimal, \n'
+    'start_date DATE,\n '
+    'end_date DATE, \n'
+    'type VARCHAR);',
+
+    'CREATE TABLE IF NOT EXISTS online_course (course_id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
+    'url VARCHAR);',
+
+    'CREATE TABLE IF NOT EXISTS onsite_course (course_id INTEGER primary key AUTOINCREMENT,\n'
+    'address VARCHAR, duartion INTEGER);',
+
+    'CREATE TABLE IF NOT EXISTS departement (departement_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
+    'departement_name VARCHAR, \n'
+    'address VARCHAR, \n'
+    'manager VARCHAR, \n'
+    'budget INTEGER);',
+
+    'CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
+    'first_name VARCHAR, \n'
+    'last_name VARCHAR, \n'
+    'start_date DATE, \n'
+    'PESEL INTEGER, \n'
+    'telephone INTEGER, \n'
+    'address VARCHAR);',
+
+    'CREATE TABLE IF NOT EXISTS stuent_grades (student_id INTEGER, \n'
+    'course_id INTEGER,\n'
+    'ects INTEGER,\n'
+    'grade INTEGER);',
+
+    'CREATE TABLE IF NOT EXISTS students (student_id INTEGER PRIMARY KEY AUTOINCREMENT, \n'
+    'first_name VARCHAR,\n'
+    'last_name VARCHAR, \n'
+    'address VARCHAR, \n'
+    'pesel INTEGER, \n'
+    'telephone INTEGER);'
+]
 
 table = ['INSERT INTO courses (title) VALUES ("Basic Excel")',
          'INSERT INTO courses (price) VALUES (350)',
